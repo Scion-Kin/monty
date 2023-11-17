4359947075dd4c38219f9dbe56e5c5d1713ddbe0
Scion-Kin <dinturner17@gmail.com>
2023-11-17 11:08:50 +0200

    Monty

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+ * main - Entry point for the Monty interpreter.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return EXIT_FAILURE;
+	}
+
+	FILE *file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return EXIT_FAILURE;
+	}
+
+	stack_t *stack = NULL;
+
+	char *line = NULL;
+	size_t len = 0;
+	unsigned int line_number = 0;
+
+	while (getline(&line, &len, file) != -1)
+	{
+		line_number++;
+		execute_opcode(&stack, line, line_number);
+	}
+
+	fclose(file);
+
+	free_stack(&stack);
+	free(line);
+
+	return EXIT_SUCCESS;
+}
