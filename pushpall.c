b22bad408d1f034e01ce294f4611d6652570785b
Scion-Kin <dinturner17@gmail.com>
2023-11-17 10:59:53 +0200

    Monty

---
+++ pushpall.c
@@ -0,0 +1,62 @@
+/* pushpall.c */
+
+#include "monty.h"
+
+/**
+ * push - puts an element on top of the stack
+ * @stack: the linked list to work with
+ * @line_number: the number of the line of the commands
+ * Return: nothing
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *arg;
+	int value;
+
+	arg = strtok(NULL, " \t\n");
+
+	if (arg == NULL || !is_numeric(arg))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	value = atoi(arg);
+
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+
+	*stack = new_node;
+}
+
+/**
+ * pall - Prints all values on the stack.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The current line number in the file.
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+
+
+	stack_t *current = *stack;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
