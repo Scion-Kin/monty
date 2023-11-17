cf2f7451dbdb1764a2f94039abef3ae736dc54fd
Scion-Kin <dinturner17@gmail.com>
2023-11-17 11:02:44 +0200

    Monty

--- pushpall.c
+++ pushpall.c
@@ -13,6 +13,7 @@ void push(stack_t **stack, unsigned int line_number)
 {
 	char *arg;
 	int value;
+	stack_t *new_node = malloc(sizeof(stack_t));
 
 	arg = strtok(NULL, " \t\n");
 
@@ -23,8 +24,6 @@ void push(stack_t **stack, unsigned int line_number)
 	}
 
 	value = atoi(arg);
-
-	stack_t *new_node = malloc(sizeof(stack_t));
 
 	if (new_node == NULL)
 	{
@@ -49,10 +48,8 @@ void push(stack_t **stack, unsigned int line_number)
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
-
-
 	stack_t *current = *stack;
+	(void)line_number;
 
 	while (current != NULL)
 	{
